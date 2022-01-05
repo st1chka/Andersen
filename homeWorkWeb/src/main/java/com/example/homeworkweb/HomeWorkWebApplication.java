660e3a233045c57b50d4e05684ec6fb664467078
st1chka <kalinine03@gmail.com>
2022-01-05 14:01:30 +0300

    first commit

---
+++ homeWorkWeb/src/main/java/com/example/homeworkweb/HomeWorkWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkWebApplication.class, args);
+    }
+
+}
---
+++ homeWorkWeb/src/test/java/com/example/homeworkweb/HomeWorkWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworkweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
